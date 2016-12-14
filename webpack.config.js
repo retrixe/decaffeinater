5cd4bc990a100e6cd06f0845b213f22ec8548376
Ibrahim Ansari <Ibrahim Ansari>
2016-12-15 01:16:53 +0530

    webpack is here, replacing gulp, but many errors..

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const config = {
+  target: "electron",
+  entry: {
+    main: "./src/main.js",
+    index: "./src/index.js"
+  },
+  output: {
+    filename: "[name].js",
+    path: __dirname + "/app",
+    sourceMapFilename: "[file].map"
+  },
+  node: {
+    __dirname: false,
+    __filename: false
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: "src/index.html", to: "index.html"}
+    ],
+    {
+      copyUnmodified: true
+    })
+  ]
+}
+
+module.exports = config;
