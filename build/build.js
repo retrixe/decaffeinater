1ee840b06307f07ef92e9dc08e2cd689834d255a
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2017-01-19 17:55:39 +0530

    flow fr node fixed and winstaller now being tested

--- build/build.js
+++ build/build.js
@@ -4,10 +4,13 @@
 var electronInstaller = require("electron-winstaller");
 
 const resultPromise = electronInstaller.createWindowsInstaller({
-  appDirectory: "/tmp/build/my-app-64",
-  outputDirectory: "/tmp/build/installer64",
-  authors: "My App Inc.",
-  exe: "myapp.exe",
+  appDirectory: ".",
+  outputDirectory: "./dist",
+  authors: "Ibrahim Ansari",
+  setupExe: "decaffeinater-installer.exe",
+  setupMsi: "decaffeinater-installer.msi",
+  iconUrl: "./build/icon.ico",
+  setupIcon: "./icon.ico",
 });
 
 resultPromise.then(() => console.log("It worked!"), e => console.log(`No dice: ${e.message}`));
