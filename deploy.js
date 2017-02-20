11fa81ab9a91f3adf412bbee32407c2572f729e5
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2017-02-20 21:53:41 +0530

    added deploy script

--- deploy.js
+++ deploy.js
@@ -8,6 +8,11 @@ being built. This script is compatible with
 for the repo ibujs/decaffeinater on GitHub.
 Update this file when you update .gitignore */
 
+// Imports.
+const fs = require("fs");
+const child_process = require("child_process");
+
+// New gitignore content on condition met.
 const newGitignoreContent = `node_modules/
 app/index.js
 app/index.js.map
@@ -15,6 +20,23 @@ app/main.js
 app/main.js.map
 app/index.html`;
 
-function editGitignore() {
+// Edit gitignore with new content with this function.
+function editGitignore(newGitignore) {
+  // Remove the git file.
+  fs.unlinkSync("./.gitignore");
 
+  // Create new gitignore.
+  fs.openSync("./.gitignore", "w+");
+
+  // Write new content to file.
+  fs.writeFileSync("./.gitignore", newGitignore, (err) => {
+    if (err) throw err;
+    child_process.execSync("git add dist/* && git commit -m \"Deploy artifacts\" && git push");
+  });
+}
+
+const branch = child_process.execSync("git rev-parse --abbrev-ref HEAD");
+
+if (branch === "deploy") {
+  editGitignore(newGitignoreContent);
 }
