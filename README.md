628baeb540d3f9a019d75299c332e556f1cc0351
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2017-04-13 17:36:44 +0530

    add material-ui and shift from semantic-ui

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # decaffeinater
-## Dependency status: [![dependencies Status](https://david-dm.org/ibujs/decaffeinater/status.svg)](https://david-dm.org/ibujs/decaffeinater) [![devDependencies Status](https://david-dm.org/ibujs/decaffeinater/dev-status.svg)](https://david-dm.org/ibujs/decaffeinater?type=dev) [![Dependency Status](https://dependencyci.com/github/ibujs/decaffeinater/badge)](https://dependencyci.com/github/ibujs/decaffeinater) [![Greenkeeper badge](https://badges.greenkeeper.io/ibujs/decaffeinater.svg)](https://greenkeeper.io/)
+## Dependency status: [![dependencies Status](https://david-dm.org/ibujs/decaffeinater/status.svg?style=flat-square)](https://david-dm.org/ibujs/decaffeinater) [![devDependencies Status](https://david-dm.org/ibujs/decaffeinater/dev-status.svg?style=flat-square)](https://david-dm.org/ibujs/decaffeinater?type=dev) [![Dependency Status](https://dependencyci.com/github/ibujs/decaffeinater/badge?style=flat-square)](https://dependencyci.com/github/ibujs/decaffeinater)
 
 ## Current build status:
 ### Windows:
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
+// @flow
 import "babel-polyfill";                     // polyfill to use async/await.
 /* eslint-disable import/no-extraneous-dependencies */
 import { ipcRenderer } from "electron";        // ipc communication to main process.
 import React from "react";
 import ReactDOM from "react-dom";
-// Following imports for semantic-ui, awsm CSS framework :D
-// eslint-disable-next-line no-unused-vars
+// Following imports for material-ui, React components based on material-ui
+/* eslint-disable no-unused-vars */
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import Paper from "material-ui/Paper";
 import { Button, Input, Progress, Segment } from "semantic-ui-react";
+/* eslint-enable */
 
 // sleep function to wait for some time.
 function sleep(s) {
@@ -13,8 +17,7 @@ function sleep(s) {
 }
 
 // Our main React component (and only one)
-// eslint-disable-next-line no-unused-vars
-class Index extends React.Component {
+export default class Index extends React.Component<any, any, any> {
   constructor(props) {
     super(props);
 
@@ -28,6 +31,7 @@ class Index extends React.Component {
     };
 
     // Bind functions here.
+    // flow-disable-next-line
     this.onStart = this.onStart.bind(this);
   }
 
@@ -38,7 +42,7 @@ class Index extends React.Component {
     // following 3 lines prevents user from tampering state during countdown.
     const currentState = JSON.parse(JSON.stringify(this.state));
     const time = (currentState.time * 60) + (currentState.hours * 3600);
-    const process = JSON.parse(JSON.stringify(this.state)).process;
+    const process = currentState.process;
     this.setState({ inProcess: true });
     // wait 1 second, then add 1 to this.state.countdown for (time) times.
     for (i = 0; i < time; i += 1) {
@@ -54,49 +58,51 @@ class Index extends React.Component {
   render() {
     const absTimer = this.state.time * 60 + this.state.hours * 3600;
     return (
-      <Segment style={{ margin: "12px" }} raised>
-        <Input
-          label="Time (in hours)"
-          type="number" fluid
-          placeholder="Insert amount of time to play in hours."
-          onChange={(e) => {
-            if (!this.state.inProcess) {
-              this.setState({ hours: e.target.value });
-            }
-          }}
-        />
-        <Input
-          label="Time (in minutes)"
-          type="number" fluid
-          placeholder="Insert amount of time to play in minutes."
-          onChange={(e) => {
-            if (!this.state.inProcess) {
-              this.setState({ time: e.target.value });
-            }
-          }}
-        />
-        <Input
-          label="Process"
-          type="text" fluid
-          placeholder="Insert the process name of the app."
-          onChange={(e) => {
-            if (!this.state.inProcess) {
-              this.setState({ process: e.target.value });
-            }
-          }}
-        />
-        <br />
-        <Button onClick={this.onStart} content="Click to start" inverted fluid color="green" />
-        <br />
-        <div />
-        <Progress
-          value={this.state.countdown}
-          total={absTimer}
-          indicating autoSuccess
-          color="teal"
-        >Time left: {absTimer - this.state.countdown} seconds left, out of {absTimer} seconds.
-        </Progress>
-      </Segment>
+      <MuiThemeProvider>
+        <Paper elevation={8}>
+          <Input
+            label="Time (in hours)"
+            type="number" fluid
+            placeholder="Insert amount of time to play in hours."
+            onChange={(e) => {
+              if (!this.state.inProcess) {
+                this.setState({ hours: e.target.value });
+              }
+            }}
+          />
+          <Input
+            label="Time (in minutes)"
+            type="number" fluid
+            placeholder="Insert amount of time to play in minutes."
+            onChange={(e) => {
+              if (!this.state.inProcess) {
+                this.setState({ time: e.target.value });
+              }
+            }}
+          />
+          <Input
+            label="Process"
+            type="text" fluid
+            placeholder="Insert the process name of the app."
+            onChange={(e) => {
+              if (!this.state.inProcess) {
+                this.setState({ process: e.target.value });
+              }
+            }}
+          />
+          <br />
+          <Button onClick={this.onStart} content="Click to start" inverted fluid color="green" />
+          <br />
+          <div />
+          <Progress
+            value={this.state.countdown}
+            total={absTimer}
+            indicating autoSuccess
+            color="teal"
+          >Time left: {absTimer - this.state.countdown} seconds left, out of {absTimer} seconds.
+          </Progress>
+        </Paper>
+      </MuiThemeProvider>
     );
   }
 }
