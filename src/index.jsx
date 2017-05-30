a88ee4105c5e84d4d97309ab7b3a7f9204519ee4
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2017-05-30 20:06:02 +0530

    minor edits

--- src/index.jsx
+++ src/index.jsx
@@ -18,33 +18,50 @@ e. ps ax is your buddy, run this command to find all running processes. If you o
 ust now, it'll be the 3rd last or so.";
 const cheatSheet2 = "1 hour = 60 minutes and 1 minute = 60 seconds. You can use both fields a\
 t once and the values will be converted and added.";
-const styleCheatSheet = {
-  marginLeft: 10,
-  marginRight: 10,
-};
 /* eslint-enable no-multi-str */
 
+// Define our style sheet.
+const indexComponentCSS = {
+  appBar: {
+    position: "relative",
+    marginBottom: 10,
+    background: "#00796B",
+  },
+  paper: {
+    marginTop: 10,
+  },
+  cheatSheetStyle: {
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  cheatSheetStyle2: {
+    margin: 10,
+  },
+}
+
 // Define main component.
-const Index = () => (
-  <MuiThemeProvider>
-    <div>
-      <AppBar style={{ position: "relative", marginBottom: 10, background: "#00796B" }}>
-        <Toolbar>
-          <Text
-            type="title" colorInherit
-          >Decaffeinater - For those who spend too much time on apps :3</Text>
-        </Toolbar>
-      </AppBar>
-      <App />
-      <Paper elevation={8} style={{ marginTop: 10 }}>
-        <br />
-        <Text type="headline" component="h3" align="center" style={styleCheatSheet}>Cheat sheet!</Text>
-        <Text type="body1" component="p" style={{ margin: 10 }}>{cheatSheet1}</Text>
-        <Text type="body1" component="p" style={styleCheatSheet}>{cheatSheet2}</Text>
-        <br />
-      </Paper>
-    </div>
-  </MuiThemeProvider>
-);
+const Index = (props, context) => {
+  return (
+    <MuiThemeProvider>
+      <div>
+        <AppBar style={indexComponentCSS.appBar}>
+          <Toolbar>
+            <Text
+              type="title" colorInherit
+            >Decaffeinater - For those who spend too much time on apps :3</Text>
+          </Toolbar>
+        </AppBar>
+        <App />
+        <Paper elevation={8} style={indexComponentCSS.paper}>
+          <br />
+          <Text type="headline" component="h3" align="center" style={indexComponentCSS.cheatSheetStyle}>Cheat sheet!</Text>
+          <Text type="body1" component="p" style={indexComponentCSS.cheatSheetStyle2}>{cheatSheet1}</Text>
+          <Text type="body1" component="p" style={indexComponentCSS.cheatSheetStyle}>{cheatSheet2}</Text>
+          <br />
+        </Paper>
+      </div>
+    </MuiThemeProvider>
+  );
+};
 
 ReactDOM.render(<Index />, document.getElementById("app"));
